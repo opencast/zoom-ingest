cfc412e74e7505b00cd3d317255e7fcf5c45d4ef
Greg Logan <gregorydlogan@gmail.com>
2021-03-19 11:54:55 -0600

    Fixing incorrect xacml flavour issue.
    This appears to be related to the order in which ingests are done.  If you
    ingest files such that a call to _do_post occurs *without* files containing
    something *after* you ingest a file containing a '+' character in its flavour
    you will see it magically disappear the + from the next post.  The major issue
    debugging this is that *this only disappears _after_ the post is sent*.  It is
    *not* visible from the Python side, but it shows up in the debugger in Java.
    
    I suspect this is the requests_toolbelt setting an incorrect content type,
    since '+' renders as ' ' depending on the content type.  Further debugging required.

--- zingest/opencast.py
+++ zingest/opencast.py
@@ -561,16 +561,17 @@ class Opencast:
             self.logger.info(f"Creating mediapackage for { rec_id }")
             mp = self._do_get(f'{ self.url }/ingest/createMediaPackage').text
             self._check_valid_mediapackage(mp)
-            self.logger.debug(f"Ingesting episode security settings for { rec_id }")
-            mp = self._do_post(f'{ self.url }/ingest/addAttachment', data={'flavor': 'security/xacml+episode', 'mediaPackage': mp}, files = {"BODY": ("ep-security.xacml", ep_acl, "text/xml") }).text
-            self._check_valid_mediapackage(mp)
+
             self.logger.debug(f"Ingesting episode dublin core settings for { rec_id }")
-            mp = self._do_post(f'{ self.url }/ingest/addDCCatalog', data={'flavor': 'dublincore/episode', 'mediaPackage': mp, 'dublinCore': ep_dc}).text
+            mp = self._do_post(f'{ self.url }/ingest/addDCCatalog', data={'flavor': 'dublincore/episode', 'mediaPackage': mp, 'dublinCore': self._prep_dublincore()}).text
             self._check_valid_mediapackage(mp)
             if eth_dc:
                 self.logger.debug(f"Ingesting episode ethterms for { rec_id }")
                 mp = self._do_post(f'{ self.url }/ingest/addDCCatalog', data={'flavor': 'ethterms/episode', 'mediaPackage': mp, 'dublinCore': eth_dc}).text
                 self._check_valid_mediapackage(mp)
+            self.logger.debug(f"Ingesting episode security settings for { rec_id }")
+            mp = self._do_post(f'{ self.url }/ingest/addAttachment', data={'flavor': 'security/xacml+episode', 'mediaPackage': mp}, files = {"BODY": ("ep-security.xacml", ep_acl, "text/xml") }).text
+            self._check_valid_mediapackage(mp)
             self.logger.info(f"Ingesting zoom video for { rec_id }")
             mp = self._do_post(f'{ self.url }/ingest/addTrack', data={'flavor': 'presentation/source', 'mediaPackage': mp}, files={ "BODY": (os.path.basename(filename), fobj, "video/mp4") }).text
             self._check_valid_mediapackage(mp)
