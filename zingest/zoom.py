aaca95f5d56b80ca6804156e4b39db14a3b8c303
Greg Logan <gregorydlogan@gmail.com>
2023-08-17 14:07:18 -0600

    Reversing the monkeypatched GDPR compliance since the upstream library now handles this correctly.
    Revert "MONKEYPATCH: Hacking in https://github.com/prschmid/zoomus/pull/188/ with some fixes, revert this after that PR is ready."
    
    This reverts commit 0fde85451ec424edf40aa19cecaca042f0dc95d2.

--- zingest/zoom.py
+++ zingest/zoom.py
@@ -8,67 +8,7 @@ import time
 from requests import HTTPError
 
 import jwt
-
-import zoomus.util
-zoomus.util.API_GDPR = "gdpr"
-
-import zoomus.client
-zoomus.client.API_BASE_URIS = {
-    zoomus.util.API_VERSION_1: "https://api.zoom.us/v1",
-    zoomus.util.API_VERSION_2: "https://api.zoom.us/v2",
-    zoomus.util.API_GDPR: "https://eu01api-www4local.zoom.us/v2",
-}
-
 from zoomus import ZoomClient
-def patched_init(
-        self,
-        api_key,
-        api_secret,
-        data_type="json",
-        timeout=15,
-        version=zoomus.util.API_VERSION_2,
-        base_uri=None,
-    ):
-        """Create a new Zoom client
-
-        :param api_key: The Zooom.us API key
-        :param api_secret: The Zoom.us API secret
-        :param data_type: The expected return data type. Either 'json' or 'xml'
-        :param timeout: The time out to use for API requests
-        :param version: The API version to use (Default is V2). The available
-                        options are API_VERSION_1 (deprecated by Zoom),
-                        or API_VERSION_2 (default)
-        :param base_uri: Set the base URI to use. By default this is chosen
-                         based on the API version chosen, but it can be
-                         overriden so that the GDPR compliant base URI can
-                         be used in the EU.
-        """
-        try:
-            base_uri = base_uri or zoomus.client.API_BASE_URIS[version]
-            self.components = zoomus.client.COMPONENT_CLASSES[version].copy()
-        except KeyError:
-            raise RuntimeError("API version not supported: %s" % version)
-
-        super(ZoomClient, self).__init__(base_uri=base_uri, timeout=timeout)
-
-        # Setup the config details
-        self.config = {
-            "api_key": api_key,
-            "api_secret": api_secret,
-            "data_type": data_type,
-            "version": version,
-            "base_uri": base_uri,
-            "token": zoomus.util.generate_jwt(api_key, api_secret),
-        }
-
-        # Instantiate the components
-        for key in self.components.keys():
-            self.components[key] = self.components[key](
-                base_uri=base_uri, config=self.config
-            )
-ZoomClient.__init__ = patched_init
-
-
 
 from zingest import db
 from zingest.common import BadWebhookData, NoMp4Files, get_config
